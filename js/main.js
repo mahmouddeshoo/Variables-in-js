f6727a997e1d8292b24da253d0ba8ec8dcba648d
mahmouddeshoo <mahmouddshoo@gmail.com>
2024-11-28 20:03:38 +0300

    Add Task files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// إنشاء المتغيرات:
+
+// أنشئ متغيرًا باسم userName وضع داخله اسمًا (مثل: “محمد”).
+// أنشئ متغيرًا باسم userAge وضع داخله رقمًا (مثل: 25).
+// أنشئ متغيرًا باسم isSubscribed وضع داخله قيمة true أو false.
+// طباعة القيم الأولى:
+
+// استخدم console.log لطباعة جملة مثل:
+// الاسم: محمد، العمر: 25، الاشتراك: فعال.
+// تعديل القيم:
+
+// عدّل قيم المتغيرات إلى اسم آخر وعمر مختلف، وقم بتغيير حالة الاشتراك.
+// إعادة الطباعة:
+
+// اطبع نفس الجملة ولكن بالقيم الجديدة.
+let userName = "Mahmoud";
+let userAge = 25;
+let isSubscribed = true;
+
+console.log("Name: ", userName,"\n","Age: ", userAge, "\n","Subscribtion: ", isSubscribed);
+console.log("=========================")
+// تعديل القيم:
+userName = "Ahmed";
+userAge = 30;
+isSubscribed = false;
+// طباعة القيم الجديدة:
+console.log("Name: ", userName,"\n","Age: ", userAge, "\n","Subscribtion: ",isSubscribed)
